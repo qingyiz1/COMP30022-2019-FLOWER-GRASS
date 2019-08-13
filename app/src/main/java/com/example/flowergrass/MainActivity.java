8ceb8323634424efe4deb6235c7cd3292faf4dfa
Qingyi Zhou <qingyiz1@student.unimelb.edu.au>
2019-08-13 16:51:59 +1000

    Login system

--- app/src/main/java/com/example/flowergrass/MainActivity.java
+++ app/src/main/java/com/example/flowergrass/MainActivity.java
@@ -3,12 +3,37 @@ package com.example.flowergrass;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button signUp,logIn;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        signUp = findViewById(R.id.signUpBtn);
+        logIn = findViewById(R.id.logInBtn);
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"You Clicked signUp", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        logIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"You Clicked logIn",Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
