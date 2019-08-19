5b8b0142bcb31417e63eec2a77b500caff7d8b17
Qingyi Zhou <qingyiz1@student.unimelb.edu.au>
2019-08-19 17:04:32 +1000

    Create second activity
    second activity after login

--- app/src/main/java/com/example/flowergrass/MainActivity.java
+++ app/src/main/java/com/example/flowergrass/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.flowergrass;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -31,7 +32,10 @@ public class MainActivity extends AppCompatActivity {
         logIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,SecondActivity.class);
+
                 Toast.makeText(MainActivity.this,"You Clicked logIn",Toast.LENGTH_LONG).show();
+                startActivity(intent);
             }
         });
 
