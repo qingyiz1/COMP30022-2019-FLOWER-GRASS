6c9313bc795c334981bdcd0955f1274a942ccff5
Qingyi Zhou <qingyiz1@student.unimelb.edu.au>
2019-09-04 21:58:46 +1000

    sign up page
    finished

--- app/src/main/java/com/example/flowergrass/ProfileActivity.java
+++ app/src/main/java/com/example/flowergrass/ProfileActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseUser;
+
 public class ProfileActivity extends AppCompatActivity {
 
     @Override
@@ -11,4 +13,17 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
     }
+
+
+    // [START on_start_check_user]
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+    // [END on_start_check_user]
+
+
 }
