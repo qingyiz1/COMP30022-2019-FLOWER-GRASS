1a5657968dcd4e164758337bf03b8cd717d688f2
GGEYE <geyeg@student.unimelb.edu.au>
2019-10-11 16:05:55 +1100

    timeline update

---
+++ app/src/main/java/com/example/flowergrass/adapter/ItemListAdapter.java
@@ -0,0 +1,79 @@
+package com.example.flowergrass.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+
+import com.example.flowergrass.R;
+import com.example.flowergrass.data.Event;
+
+import java.util.ArrayList;
+
+
+public class ItemListAdapter extends ArrayAdapter<Event> {
+
+    private static final String TAG = "EventListAdapter";
+
+    private Context mContext;
+    private int mResource;
+    private int lastPosition = -1;
+
+    /**
+     * Holds variables in a View
+     */
+    private static class ViewHolder {
+        TextView name;
+        TextView date;
+        TextView details;
+    }
+
+
+    public ItemListAdapter(Context context, int resource, ArrayList<Event> objects) {
+        super(context, resource, objects);
+        mContext = context;
+        mResource = resource;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        //get event information
+        String title = getItem(position).getTitle();
+        String date = getItem(position).getDate();
+        String details = getItem(position).getDetails();
+
+        //Create the Event object with the information
+        Event event = new Event(title,date,details);
+
+        //ViewHolder object
+        ViewHolder holder;
+
+        if(convertView == null){
+            LayoutInflater inflater = LayoutInflater.from(mContext);
+            convertView = inflater.inflate(mResource, parent, false);
+            holder= new ViewHolder();
+            holder.name = convertView.findViewById(R.id.EventName);
+            holder.date = convertView.findViewById(R.id.EventDate);
+            holder.details = convertView.findViewById(R.id.EventDetails);
+            convertView.setTag(holder);
+        }
+        else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        holder.name.setText(event.getTitle());
+        holder.date.setText(event.getDate());
+        holder.details.setText(event.getDetails());
+
+
+        return convertView;
+    }
+
+
+
+}
--- app/src/main/java/com/example/flowergrass/fragments/SecondFragment.java
+++ app/src/main/java/com/example/flowergrass/fragments/SecondFragment.java
@@ -2,8 +2,11 @@ package com.example.flowergrass.fragments;
 
 
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,36 +17,93 @@ import android.view.animation.AnimationSet;
 import android.view.animation.AnimationUtils;
 import android.view.animation.DecelerateInterpolator;
 import android.widget.ImageView;
+import android.widget.ListView;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import com.example.flowergrass.R;
+import com.example.flowergrass.adapter.EventListAdapter;
+import com.example.flowergrass.data.Event;
+import com.example.flowergrass.utils.CirclePageIndicator;
+import com.example.flowergrass.utils.GlideApp;
+import com.example.flowergrass.utils.PageIndicator;
+import com.google.firebase.Timestamp;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.util.ArrayList;
 
 
 public class SecondFragment extends Fragment {
-    ImageView demoImage;
+    //ImageView demoImage;
     int imagesToShow[] = { R.drawable.avatar_girl, R.drawable.avatar_girl3,R.drawable.avatar_girl2 };
     Handler handler;
     private Runnable animator;
     private static final long ANIM_DELAY = 10000;
 
+
+    public static final String ARG_ITEM_ID = "home_fragment";
+
+    private static final long ANIM_VIEWPAGER_DELAY = 10000;
+    private static final long ANIM_VIEWPAGER_DELAY_USER_VIEW = 10000;
+    private static final String TAG = "HOME_FRAGMENT";
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    // UI References
+    private ListView mItemListView;
+    //TextView imgNameTxt;
+    PageIndicator mIndicator;
+
+    AlertDialog alertDialog;
+
+    //Data - Temporary
+    int[] products ={R.drawable.data1,R.drawable.data2,R.drawable.data3};
+    ArrayList<Event> events = new ArrayList<>();
+
+
+    FragmentActivity activity;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        activity = getActivity();
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fg2, container, false);
-        demoImage = view.findViewById(R.id.timeline_img);
+        Init(view);
+        //loadWithGlide(view,R.id.);
         return view;
     }
 
+    //Initialise Image slider and EventListView
+    private void Init(View view) {
+        //mViewPager = view.findViewById(R.id.view_pager);
+        mItemListView = view.findViewById(R.id.ItemListView);
+        getEventsData();
+        //mIndicator = (CirclePageIndicator) view.findViewById(R.id.indicator);
+        //mIndicator.setOnPageChangeListener(new HomeFragment.PageChangeListener());
+    }
 
     public void runnable() {
         handler = new Handler();
         animator = new Runnable() {
             public void run() {
-                animate(demoImage, imagesToShow, 0, false);
+                //animate(demoImage, imagesToShow, 0, false);
                 handler.postDelayed(animator, ANIM_DELAY);
             }
         };
-
     }
 
     @Override
@@ -62,53 +122,93 @@ public class SecondFragment extends Fragment {
         }
     }
 
-    private void animate(final ImageView imageView, final int images[], final int imageIndex, final boolean forever) {
 
-        //imageView <-- The View which displays the images
-        //images[] <-- Holds R references to the images to display
-        //imageIndex <-- index of the first image to show in images[]
-        //forever <-- If equals true then after the last image it starts all over again with the first image resulting in an infinite loop. You have been warned.
+    public void showAlertDialog(String message, final boolean finish) {
+        alertDialog = new AlertDialog.Builder(activity).create();
+        alertDialog.setMessage(message);
+        alertDialog.setCancelable(false);
 
-        int fadeInDuration = 500; // Configure time values here
-        int timeBetween = 3000;
-        int fadeOutDuration = 1000;
-
-        imageView.setVisibility(View.INVISIBLE);    //Visible or invisible by default - this will apply when the animation ends
-        imageView.setImageResource(images[imageIndex]);
-
-        Animation fadeIn = new AlphaAnimation(0, 1);
-        fadeIn.setInterpolator(new DecelerateInterpolator()); // add this
-        fadeIn.setDuration(fadeInDuration);
-
-        Animation fadeOut = new AlphaAnimation(1, 0);
-        fadeOut.setInterpolator(new AccelerateInterpolator()); // and this
-        fadeOut.setStartOffset(fadeInDuration + timeBetween);
-        fadeOut.setDuration(fadeOutDuration);
-
-        AnimationSet animation = new AnimationSet(false); // change to false
-        animation.addAnimation(fadeIn);
-        animation.addAnimation(fadeOut);
-        animation.setRepeatCount(1);
-        imageView.setAnimation(animation);
-
-        animation.setAnimationListener(new Animation.AnimationListener() {
-            public void onAnimationEnd(Animation animation) {
-                if (images.length - 1 > imageIndex) {
-                    animate(imageView, images, imageIndex + 1,forever); //Calls itself until it gets to the end of the array
-                }
-                else {
-                    if (forever){
-                        animate(imageView, images, 0,forever);  //Calls itself to start the animation all over again in a loop if forever = true
+        alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "OK",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        if (finish)
+                            activity.finish();
                     }
-                }
-            }
-            public void onAnimationRepeat(Animation animation) {
-                // TODO Auto-generated method stub
-            }
-            public void onAnimationStart(Animation animation) {
-                // TODO Auto-generated method stub
-            }
-        });
+                });
+        alertDialog.show();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+
+    public void loadWithGlide(View container, int ID) {
+        // [START storage_load_with_glide]
+        // Reference to an image file in Cloud Storage
+        StorageReference storageReference = FirebaseStorage.getInstance().getReference().child("images/test.jpg");
+
+        // ImageView in your Activity
+        ImageView imageView = container.findViewById(ID);
+
+        // Download directly from StorageReference using Glide
+        // (See MyAppGlideModule for Loader registration)
+        GlideApp.with(this /* context */)
+                .load(storageReference)
+                .into(imageView);
+        // [END storage_load_with_glide]
+    }
+
+
+    public void getEventsData(){
+        db.collection("events")
+                .orderBy("dateCreated", Query.Direction.DESCENDING).limit(10)
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot value,
+                                        @Nullable FirebaseFirestoreException e) {
+                        if (e != null) {
+                            Log.w(TAG, "Listen failed.", e);
+                            return;
+                        }
+
+                        for (QueryDocumentSnapshot doc : value) {
+                            Timestamp date = (Timestamp)doc.getData().get("dateCreated");
+
+                            events.add(new Event(doc.getString("title"),date.toDate().toString(),doc.getString("details")));
+
+                        }
+                        mItemListView.setAdapter(new EventListAdapter(getActivity().getApplicationContext(),R.layout.adapter_view_layout,events));
+                        Log.d(TAG, "Current events in : " + events.toString());
+                    }
+
+                });
+    }
+
+    public void getProductsData(){
+        db.collection("events")
+                .orderBy("dateCreated", Query.Direction.DESCENDING).limit(10)
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot value,
+                                        @Nullable FirebaseFirestoreException e) {
+                        if (e != null) {
+                            Log.w(TAG, "Listen failed.", e);
+                            return;
+                        }
+
+                        for (QueryDocumentSnapshot doc : value) {
+                            Timestamp date = (Timestamp)doc.getData().get("dateCreated");
+
+                            events.add(new Event(doc.getString("title"),date.toDate().toString(),doc.getString("details")));
+
+                        }
+                        mItemListView.setAdapter(new EventListAdapter(getActivity().getApplicationContext(),R.layout.adapter_view_layout,events));
+                        Log.d(TAG, "Current events in : " + events.toString());
+                    }
+
+                });
     }
 
 
