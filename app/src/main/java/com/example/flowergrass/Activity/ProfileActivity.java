1fc0662719142aa2c28fab78451125dc89449252
Qingyi Zhou <qingyiz1@student.unimelb.edu.au>
2019-10-21 22:16:17 +1100

    fg4

--- app/src/main/java/com/example/flowergrass/Activity/ProfileActivity.java
+++ app/src/main/java/com/example/flowergrass/Activity/ProfileActivity.java
@@ -14,7 +14,7 @@ public class ProfileActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
+        setContentView(R.layout.fg4);
         mAuth = FirebaseAuth.getInstance();
     }
 
